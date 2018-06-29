5da514e2a210d53fe34d3eb74784178de52f4052
box <boxknight@outlook.com>
2018-06-29 16:54:02 +0800

    登录、个人注册

---
+++ js/login.js
@@ -0,0 +1,31 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Fullscreen background
+    */
+    //$.backstretch("/img/background1.jpg");
+
+    /*
+        Form validation
+    */
+    $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
+        $(this).removeClass('input-error');
+    });
+
+    $('.registration-form').on('submit', function(e) {
+
+        $(this).find('input[type="text"], textarea').each(function(){
+            if( $(this).val() == "" ) {
+                e.preventDefault();
+                $(this).addClass('input-error');
+            }
+            else {
+                $(this).removeClass('input-error');
+            }
+        });
+
+    });
+
+
+});
