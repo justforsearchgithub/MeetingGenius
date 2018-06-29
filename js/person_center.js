ccd7974932b3f2b5b29c5e146246f27a1cfc20ed
Stalkker <bhrgyt@163.com>
2018-06-29 23:09:47 +0800

    个人中心

---
+++ js/person_center.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var vm = new Vue({
+        el: '#per_info',
+        data: {
+            is_account: true,
+            is_collection: false,
+            is_childaccount: false,
+            is_password: false,
+            account_name:"Tom"
+        }
+    });
+}
