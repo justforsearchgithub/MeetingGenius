6dbe5f3e0967d1de2f2008304c18fa550eda71c7
Stalkker <bhrgyt@163.com>
2018-06-30 20:54:18 +0800

    个人中心

--- js/person_center.js
+++ js/person_center.js
@@ -1,13 +1,74 @@
 $(document).ready(function () {
+
     var vm = new Vue({
         el: '#per_info',
         data: {
-            is_account: true,
-            is_collection: false,
-            is_childaccount: false,
-            is_password: false,
+            is_normal:false,
+            not_normal:true,
             account_name:'Tom',
-            account_mail:'123@163.com'
+            account_mail:'123@163.com',
+            childaccounts:[
+                {
+                    name:'456@163.com',
+                    password:'1111'
+                }
+            ]
+        },
+        methods:{
+            click_account: function (event) {
+                document.getElementById("div_account").style.display="block";
+                document.getElementById("div_collection").style.display="none";
+                document.getElementById("div_childaccount").style.display="none";
+                document.getElementById("div_password").style.display="none";
+            },
+            click_collection: function (event) {
+                document.getElementById("div_account").style.display="none";
+                document.getElementById("div_collection").style.display="block";
+                document.getElementById("div_childaccount").style.display="none";
+                document.getElementById("div_password").style.display="none";
+            },
+            click_childaccount: function (event) {
+                document.getElementById("div_account").style.display="none";
+                document.getElementById("div_collection").style.display="none";
+                document.getElementById("div_childaccount").style.display="block";
+                document.getElementById("div_password").style.display="none";
+             },
+            click_password: function (event) {
+                document.getElementById("div_account").style.display="none";
+                document.getElementById("div_collection").style.display="none";
+                document.getElementById("div_childaccount").style.display="none";
+                document.getElementById("div_password").style.display="block";
+           }
+
         }
-    })
+    });
+
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+    trigger.click(function () {
+        hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+
+
 });
